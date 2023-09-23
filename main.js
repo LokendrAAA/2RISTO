019afacef43e251ce5056b341230290696596a2b
Lokendra Singh Shekhawat <123613608+LokendrAAA@users.noreply.github.com>
2023-09-24 00:14:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -54,3 +54,12 @@
             }
         }
     
+       
+        function resetCheckboxes() {
+            var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            checkboxes.forEach(function (checkbox) {
+                checkbox.checked = false;
+            });
+        }
+        
+       
